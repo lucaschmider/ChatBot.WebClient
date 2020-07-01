837224cd954a4afd2c6ccd6549b4838aeb9816dd
Luca Schmider <luca.schmider@gmail.com>
2020-07-01 20:45:38 +0200

    added message design

--- src/app/chat/chat-message/chat-message.component.ts
+++ src/app/chat/chat-message/chat-message.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.scss']
 })
-export class ChatMessageComponent implements OnInit {
+export class ChatMessageComponent implements OnDestroy {
+  @Input() public isIncoming: Observable<boolean>;
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
+  }
+
+  public ngOnDestroy(): void {
+
   }
 
 }
