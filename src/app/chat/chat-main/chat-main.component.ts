64ce69008041461d7c3aadd970265a6b31804cf3
Luca Schmider <luca.schmider@gmail.com>
2020-07-05 20:55:55 +0200

    Introduced auto scroll

--- src/app/chat/chat-main/chat-main.component.ts
+++ src/app/chat/chat-main/chat-main.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-chat-main',
   templateUrl: './chat-main.component.html',
   styleUrls: ['./chat-main.component.scss']
 })
-export class ChatMainComponent implements OnInit {
+export class ChatMainComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
