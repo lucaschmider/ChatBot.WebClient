52c848ed22e08c0cbc90619d80a8c992bff558d3
Luca Schmider <luca.schmider@gmail.com>
2020-07-08 19:55:34 +0200

    Created login component

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { UserComponent } from './user.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: "login" }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
