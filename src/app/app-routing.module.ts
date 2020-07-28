9bcee20e64667b0f0d943e43a3c66c4d2376e09b
Luca Schmider <luca.schmider@gmail.com>
2020-07-28 08:48:57 +0200

    added admin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "./shared/auth.guard";
-
+import { AdminGuard } from "./shared/admin.guard";
 const routes: Routes = [
   {
     path: "chat",
@@ -9,7 +9,7 @@ const routes: Routes = [
     canActivateChild: [AuthGuard]
   },
   { path: "user", loadChildren: () => import("./user/user.module").then((m) => m.UserModule) },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivateChild: [AuthGuard, AdminGuard] },
   { path: "", redirectTo: "chat", pathMatch: "full" },
 ];
 
---
+++ src/app/shared/admin.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router, CanActivateChild } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./services/auth.service";
+import { take, map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService) { }
+  canActivateChild(): Observable<boolean> {
+    return this.authService.user$.pipe(
+      take(1),
+      map((user) => !!user && user.isAdmin)
+    );
+  }
+  canActivate(): Observable<boolean> {
+    return this.authService.user$.pipe(
+      take(1),
+      map((user) => !!user && user.isAdmin)
+    );
+  }
+
+}
