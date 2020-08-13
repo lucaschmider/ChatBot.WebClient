2abce03e6b9303bc82349d322192c960bba304bf
Luca Schmider <luca.schmider@gmail.com>
2020-08-13 07:00:35 +0200

    updated login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../../shared/services/auth.service";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatDialog } from '@angular/material/dialog';
+import { LoadingSpinnerComponent } from 'src/app/shared/loading-spinner/loading-spinner.component';
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
@@ -9,17 +11,26 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 })
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
-  constructor(private authService: AuthService, private snackBar: MatSnackBar) {
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl("", [Validators.required, Validators.email]),
       password: new FormControl("", Validators.required)
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   public async login(): Promise<void> {
+    if (this.loginForm.invalid)
+      return;
+
+    const dialogRef = this.dialog.open(LoadingSpinnerComponent, { hasBackdrop: false });
     const loginResult = await this.authService.signIn(this.loginForm.value);
+    dialogRef.close();
     if (!loginResult.wasSuccessful) {
       this.snackBar.open(loginResult.errorCode, null, {
         duration: 3000
